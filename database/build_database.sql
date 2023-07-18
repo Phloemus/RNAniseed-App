7289fd29f7d356773eb311389f517911cc2027c2
Phloemus <brieucquemeneur@gmail.com>
2023-07-18 12:01:28 +0200

    build: creation of a procedure allowing to choose the schema to create within the database and create tables as documented for this new schema

--- database/build_database.sql
+++ database/build_database.sql
@@ -19,72 +19,106 @@
 
 -- Create one of the many schema to create (a schema by specie is planned. Only a  
 -- mammilata schema is currently created and used)
-CREATE SCHEMA IF NOT EXISTS phmamm;
+CREATE OR REPLACE FUNCTION build_new_schema(schema_name varchar(32)) RETURNS void AS $body$
+DECLARE
+	link_expression_to_cell INT;
+	link_expression_to_gene INT;
+	link_cell_to_embryo INT;
+BEGIN
+
+	-- Create the shema wanted by the user using the 
+	-- logged user as owner of the schema
+	EXECUTE format('CREATE SCHEMA IF NOT EXISTS %I', schema_name);
+
+	-- Create the tables of the database
+	EXECUTE format('CREATE TABLE IF NOT EXISTS %I.gene (
+		id SERIAL PRIMARY KEY,
+		unique_gene_id VARCHAR(9) NOT NULL,
+		aniseed_id VARCHAR(30) NOT NULL,
+		ena_id VARCHAR(30),
+		go_biological_process_id VARCHAR(11),
+		go_molecular_function_id VARCHAR(11),
+		is_characterized NUMERIC(1)
+	)', schema_name);
+
+	EXECUTE format('CREATE TABLE IF NOT EXISTS %I.cell (
+		id SERIAL PRIMARY KEY,
+		fk_embryo_id INT NOT NULL,
+		ao_territories_id VARCHAR(20)
+	)', schema_name);
+
+	EXECUTE format('CREATE TABLE IF NOT EXISTS %I.expression (
+		id SERIAL PRIMARY KEY,
+		fk_cell_id INT NOT NULL,
+		fk_gene_id INT NOT NULL,
+		expression_level INT NOT NULL
+	)', schema_name);
+
+	EXECUTE format('CREATE TABLE IF NOT EXISTS %I.embryo (
+		id SERIAL PRIMARY KEY,
+		is_wild_type NUMERIC(1)
+	)', schema_name);
+
+	-- Addition of comments on the tables
+	EXECUTE format('COMMENT ON TABLE %I.gene
+    		IS "Represent a gene independantly from any gene models"', schema_name);
+
+	EXECUTE format('COMMENT ON TABLE %I.cell
+    		IS "Represent a cell in which its transcriptome has been sequenced. A cell here is considered as a group of expression levels"', schema_name);
+
+	EXECUTE fromat('COMMENT ON TABLE %I.expression
+    		IS "Represent the expression level of every genes for each cells"', schema_name);
+
+	EXECUTE format('COMMENT ON TABLE %I.embryo
+    		IS "Represents a group of cells. Embryos may have any number of cells but at least 2"', schema_name);
+
+	-- Store the existance of each constrains used within the database
+	EXECUTE format('SELECT 1 FROM pg_constraint
+        	WHERE conname = ''link_expression_to_cell''
+                AND conrelid = %I''.expression''::regclass', schema_name)
+	INTO link_expression_to_cell;
+
+	EXECUTE format('SELECT 1 FROM pg_constraint
+                WHERE conname = ''link_expression_to_gene''
+                AND conrelid = %I''.expression''::regclass', schema_name)
+	INTO link_expression_to_gene;
+
+	EXECUTE format('SELECT 1 FROM pg_constraint
+                        WHERE conname = ''link_cell_to_embryo''
+                        AND conrelid = %I''.cell''::regclass', schema_name)
+	INTO link_cell_to_embryo;
 
 
--- Create the tables of the database
-CREATE TABLE IF NOT EXISTS gene (
-	id SERIAL PRIMARY KEY,
-	unique_gene_id VARCHAR(9) NOT NULL,
-	aniseed_id VARCHAR(30) NOT NULL,
-	ena_id VARCHAR(30),
-	go_biological_process_id VARCHAR(11),
-	go_molecular_function_id VARCHAR(11),
-	is_characterized NUMERIC(1)
-);
+	-- Create the links between the tables by adding constraints if they aren't binded to the tables yet
+	IF link_expression_to_cell IS NULL THEN
+    		EXECUTE format('ALTER TABLE %I.expression ADD CONSTRAINT link_expression_to_cell
+			FOREIGN KEY(fk_cell_id)
+			REFERENCES %I.cell(id)', schema_name, schema_name);
+  	END IF;
 
-CREATE TABLE IF NOT EXISTS cell (
-	id SERIAL PRIMARY KEY,
-	fk_embryo_id INT NOT NULL,
-	ao_territories_id VARCHAR(20)
-);
+	IF link_expression_to_gene IS NULL THEN
+		EXECUTE format('ALTER TABLE %I.expression ADD CONSTRAINT link_expression_to_gene
+			FOREIGN KEY(fk_gene_id)
+			REFERENCES %I.gene(id)', schema_name, schema_name);
+	END IF;
 
-CREATE TABLE IF NOT EXISTS expression (
-	id SERIAL PRIMARY KEY,
-	fk_cell_id INT NOT NULL,
-	fk_gene_id INT NOT NULL,
-	expression_level INT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS embryo (
-	id SERIAL PRIMARY KEY,
-	is_wild_type NUMERIC(1)
-);
-
--- Addition of comments on the tables
-COMMENT ON TABLE gene
-    IS 'Represent a gene independantly from any gene models';
-
-COMMENT ON TABLE cell
-    IS 'Represent a cell in which its transcriptome has been sequenced. A cell here is considered as a group of expression levels';
-
-COMMENT ON TABLE expression
-    IS 'Represent the expression level of every genes for each cells';
-
-COMMENT ON TABLE embryo
-    IS 'Represents a group of cells. Embryos may have any number of cells but at least 2';
+	IF link_cell_to_embryo IS NULL THEN
+		EXECUTE format('ALTER TABLE %I.cell ADD CONSTRAINT link_cell_to_embryo
+			FOREIGN KEY(fk_embryo_id)
+			REFERENCES %I.embryo(id), schema_name, schema_name);
+	END IF;
 
 
--- Create the links between the tables by adding constraints
-ALTER TABLE expression ADD CONSTRAINT link_expression_to_cell
-	FOREIGN KEY(fk_cell_id)
-        REFERENCES cell(id);
+	-- Addition of comments on the table constraints
+	EXECUTE format('COMMENT ON CONSTRAINT link_expression_to_cell ON %I.expression
+    		IS "Constraint linking the expression level to a cell"', schema_name);
 
-ALTER TABLE expression ADD CONSTRAINT link_expression_to_gene
-	FOREIGN KEY(fk_gene_id)
-	REFERENCES gene(id);
+	EXECUTE format('COMMENT ON CONSTRAINT link_expression_to_gene ON %I.expression
+    		IS 'Constraint linking the expression level to a gene"', schema_name);
 
-ALTER TABLE cell ADD CONSTRAINT link_cell_to_embryo
-	FOREIGN KEY(fk_embryo_id)
-	REFERENCES embryo(id);
+	EXECUTE format('COMMENT ON CONSTRAINT link_cell_to_embryo ON %I.cell
+    		IS "Constraint linking the cell with its origin embryo"', schema_name);
 
+	RETURN;
 
--- Addition of comments on the table constraints
-COMMENT ON CONSTRAINT link_expression_to_cell ON expression
-    IS 'Constraint linking the expression level to a cell';
-
-COMMENT ON CONSTRAINT link_expression_to_gene ON expression
-    IS 'Constraint linking the expression level to a gene';
-
-COMMENT ON CONSTRAINT link_cell_to_embryo ON cell
-    IS 'Constraint linking the cell with its origin embryo';
+END $body$ language plpgsql;
